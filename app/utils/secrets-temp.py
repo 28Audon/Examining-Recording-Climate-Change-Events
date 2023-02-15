0bbda92a5963411340821fb11652c75f826bb8d5
Your Na <you@example.com>
2023-02-15 09:29:39 -0800

    efrew

--- app/utils/secrets-temp.py
+++ app/utils/secrets-temp.py
@@ -2,11 +2,10 @@ import os
 
 def getSecrets():
     secrets = {
-        'MONGO_HOST':'mongodb+srv://28Audon:<password>@cluster0.7hop0b3.mongodb.net/?retryWrites=true&w=majority',
+        'MONGO_HOST':'mongodb+srv://28Audon:<mwGbHL5q1abWLxcl>@cluster0.7hop0b3.mongodb.net/?retryWrites=true&w=majority',
         'MONGO_DB_NAME':'',
         'GOOGLE_CLIENT_ID': '',
         'GOOGLE_CLIENT_SECRET':'',
         'GOOGLE_DISCOVERY_URL':"https://accounts.google.com/.well-known/openid-configuration"
         }
     return secrets
-    #mwGbHL5q1abWLxcl
