24fa06d4208dee5af51dccbee1311e220d463c2a
28Audon <28audon@gmail.com>
2023-02-20 18:08:18 -0800

    787

--- app/utils/secrets-temp.py
+++ app/utils/secrets-temp.py
@@ -4,8 +4,8 @@ def getSecrets():
     secrets = {
         'MONGO_HOST':'',
         'MONGO_DB_NAME':'',
-        'GOOGLE_CLIENT_ID': '',
-        'GOOGLE_CLIENT_SECRET':'',
+        'GOOGLE_CLIENT_ID': '156656430524-83s2o73ontsca6e2004valk279e1saea.apps.googleusercontent.com',
+        'GOOGLE_CLIENT_SECRET':'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',
         'GOOGLE_DISCOVERY_URL':"https://accounts.google.com/.well-known/openid-configuration"
         }
     return secrets
