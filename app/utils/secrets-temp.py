d7d45cfdc7ce9c1d6fd4d72012c9f8dca29df2f1
28Audon <28audon@gmail.com>
2023-03-10 10:15:09 -0800

    654

--- app/utils/secrets-temp.py
+++ app/utils/secrets-temp.py
@@ -2,8 +2,8 @@ import os
 
 def getSecrets():
     secrets = {
-        'MONGO_HOST':'mongodb+srv://28Audon:<password>@cluster0.7hop0b3.mongodb.net/?retryWrites=true&w=majority',
-        'MONGO_DB_NAME':'',
+        'MONGO_HOST':'mongodb+srv://28Audon:mzFopj5QoLziauqx@cluster0.7hop0b3.mongodb.net/RecordingEvents?retryWrites=true&w=majority',
+        'MONGO_DB_NAME':'RecordingEvents',
         'GOOGLE_CLIENT_ID': '156656430524-83s2o73ontsca6e2004valk279e1saea.apps.googleusercontent.com',
         'GOOGLE_CLIENT_SECRET':'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',
         'GOOGLE_DISCOVERY_URL':"https://accounts.google.com/.well-known/openid-configuration"
