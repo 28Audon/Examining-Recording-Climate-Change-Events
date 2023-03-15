e4c3e42eefecc96d048c183d0269cf48b0fd5a28
28Audon <28Audon@gmail.com>
2023-03-15 10:43:09 -0700

    65460

--- app/routes/default.py
+++ app/routes/default.py
@@ -9,3 +9,27 @@ def index():
 @app.route('/aboutus')
 def aboutus():
     return render_template('aboutus.html')
+
+@app.route('/base.html')
+def index():
+    return render_template('base.html')
+
+@app.route('/blank_form.html')
+def index():
+    return render_template('blank_form.html')
+
+@app.route('/blank.html')
+def index():
+    return render_template('blank.html')
+@app.route('/blogform.html')
+
+def index():
+    return render_template('blogform.html')
+
+@app.route('/blog.html')
+def index():
+    return render_template('blog.html')
+
+@app.route('/commentfrom.html')
+def index():
+    return render_template('commentfrom.html')
