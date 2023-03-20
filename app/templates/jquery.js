ecd778796ad31b907e1b29661b747da56d711b10
28Audon <28audon@gmail.com>
2023-03-19 21:31:06 -0700

    rujhtyh

--- app/templates/jquery.js
+++ app/templates/jquery.js
@@ -1,24 +1,7 @@
-<!doctype html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <title>load demo</title>
-  <style>
-  body {
-    font-size: 12px;
-    font-family: Arial;
-  }
-  </style>
-  <script src="https://code.jquery.com/jquery-3.6.3.js"></script>
-</head>
-<body>
- 
-<b>Projects:</b>
-<ol id="new-projects"></ol>
- 
-<script>
-$( "#new-projects" ).load( "/resources/load.html #projects li" );
-</script>
- 
-</body>
-</html>
+$(function () {
+    var includes = $('[data-include]')
+    $.each(includes, function () {
+      var file = 'views/' + $(this).data('include') + '.html'
+      $(this).load(file)
+    })
+  })
