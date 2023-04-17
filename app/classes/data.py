e04db70df87e81ba6a6e236b4e77fc144b1e561f
Your Na <you@example.com>
2023-04-17 10:21:12 -0700

    546

--- app/classes/data.py
+++ app/classes/data.py
@@ -90,20 +90,3 @@ class Comment(Document):
             'ordering': ['-createdate']
         }
     #New<---
-
-    class Location(Document):
-        author = ReferenceField('User',reverse_delete_rule=CASCADE) 
-        createdate = DateTimeField(default=dt.datetime.utcnow)
-        modifydate = DateTimeField()
-        name = StringField()
-        streetAddress = StringField()
-        city = StringField()
-        state = StringField()
-        zipcode = StringField()
-        description = StringField()
-        lat = FloatField()
-        lon = FloatField()
-        
-        meta = {
-            'ordering': ['-createdate']
-            }
