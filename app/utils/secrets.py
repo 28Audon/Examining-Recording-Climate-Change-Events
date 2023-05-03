7caf38817e8028ef7f048777c8b57787b965c9df
Your Na <you@example.com>
2023-05-03 10:18:32 -0700

    53

--- app/utils/secrets.py
+++ app/utils/secrets.py
@@ -1,21 +1,13 @@
 import os
-from dotenv import load_dotenv
-
-
-
-
-load_dotenv()
-
-
 
 
 def getSecrets():
-  secrets = {
-      'MONGO_HOST': os.getenv("MONGO_HOST"),
-      'MONGO_DB_NAME': os.getenv("MONGO_DB_NAME"),
-      'GOOGLE_CLIENT_ID': os.getenv("GOOGLE_CLIENT_ID"),
-      'GOOGLE_CLIENT_SECRET': os.getenv("GOOGLE_CLIENT_SECRET"),
-      'GOOGLE_DISCOVERY_URL':"https://accounts.google.com/.well-known/openid-configuration",
-      'MY_EMAIL_ADDRESS': os.getenv("MY_EMAIL_ADDRESS"),
-  }
-  return secrets
+    secrets = {
+        'MONGO_HOST':'mongodb+srv://28Audon:mzFopj5QoLziauqx@cluster0.7hop0b3.mongodb.net/RecordingEvents?retryWrites=true&w=majority',
+        'MONGO_DB_NAME':'RecordingEvents',
+        'GOOGLE_CLIENT_ID': '156656430524-83s2o73ontsca6e2004valk279e1saea.apps.googleusercontent.com',
+        'GOOGLE_CLIENT_SECRET':'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',
+        'GOOGLE_DISCOVERY_URL':"https://accounts.google.com/.well-known/openid-configuration",
+        'MY_EMAIL_ADDRESS':'s_audon.padillatorres@ousd.org',
+        }
+    return secrets
